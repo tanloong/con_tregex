21596e18265c326a7763e3d5d3fdd691b69c1182
Tan Long <tanloong@foxmail.com>
2023-07-07 02:56:58 +0800

    fix: avoid E402 (module level import not at top of file)
    https://stackoverflow.com/questions/56184551/add-path-to-sys-path-vs-pep-e402

--- src/pytregex/__init__.py
+++ src/pytregex/__init__.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding=utf-8 -*-
 
-import os
-import sys
-
-project_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, project_dir)
+import _path
 
 from main import main
 
---
+++ src/pytregex/_path.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+import os
+import sys
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, project_dir)
